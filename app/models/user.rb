836b25438a05fe2360bf7cb547c7eadbb51c2add
mahdi@EshopGuide <k.mahdi99@gmail.com>
2025-03-16 13:40:36 +0100

    Rubocop.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,34 +6,34 @@ class User < ApplicationRecord
 
   # Geocoding configuration
   geocoded_by :full_address do |obj, results|
-    if geo = results.first
+    if geo == results.first
       obj.latitude = geo.latitude
       obj.longitude = geo.longitude
-      
+
       # Get address components from Nominatim
-      address = geo.data['address']
-      obj.federal_state = address['state']
-      obj.country = address['country']
-      obj.postal_code = address['postcode']
-      obj.district = address['city_district'] || address['county'] || address['suburb']
-      obj.formatted_address = geo.data['display_name']
+      address = geo.data["address"]
+      obj.federal_state = address["state"]
+      obj.country = address["country"]
+      obj.postal_code = address["postcode"]
+      obj.district = address["city_district"] || address["county"] || address["suburb"]
+      obj.formatted_address = geo.data["display_name"]
     end
   end
-  
-  after_validation :geocode, if: ->(obj) { 
+
+  after_validation :geocode, if: ->(obj) {
     !Rails.env.test? && (obj.street_changed? || obj.city_changed? || obj.zip_changed?)
   }
 
   # Validations for address fields
   validates :street, :city, :zip, presence: true
 
-  validates :latitude, numericality: { 
+  validates :latitude, numericality: {
     greater_than_or_equal_to: -90,
     less_than_or_equal_to: 90,
     message: "must be between -90 and 90"
   }, allow_nil: true
 
-  validates :longitude, numericality: { 
+  validates :longitude, numericality: {
     greater_than_or_equal_to: -180,
     less_than_or_equal_to: 180,
     message: "must be between -180 and 180"
@@ -42,6 +42,6 @@ class User < ApplicationRecord
   private
 
   def full_address
-    [street, city, zip, "Germany"].compact.join(', ')
+    [ street, city, zip, "Germany" ].compact.join(", ")
   end
 end
